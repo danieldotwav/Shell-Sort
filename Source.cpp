23a7a167d94161c156f7250ae28fbf61938fde05
Daniel Rivas <drivas8891@gmail.com>
2024-05-16 21:15:18 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <array>
+#include <random>
+using namespace std;
+
+const int SMALL = 15;
+const int LARGE = 50;
+const int RANGE = 20;
+
+int* generateRandomArrayOfSize(int size);
+void printArray(int* a, int size);
+
+int main() {
+	// Use the current time as a seed for the random generator
+	srand(time(0));
+
+	// Generate a random array of size 15
+	int* my_arr = generateRandomArrayOfSize(15);
+
+	// Print array to verify randomness
+	printArray(my_arr, 15);
+
+    // Free up dynamically allocated memory
+    delete[] my_arr;
+
+	return 0;
+}
+
+// This function takes in 
+int* generateRandomArrayOfSize(int size) {
+    if (size == SMALL) {
+        // Dynamically allocate an array of size 15
+        int* arr = new int[SMALL];
+
+        // Generate 15 random values between 0 and 20
+        for (int i = 0; i < SMALL; ++i) {
+            arr[i] = rand() % RANGE;
+        }
+
+        return arr;
+    }
+    else if (size == LARGE) {
+        // Dynamically allocate an array of size 50
+        int* arr = new int[LARGE];
+
+        // Generate 50 random values between 0 and 20
+        for (int i = 0; i < LARGE; ++i) {
+            arr[i] = rand() % RANGE;
+        }
+
+        return arr;
+    }
+    else {
+        cout << "Error: Unable to determine array size. Function parameter should be either SMALL or LARGE." << endl;
+        return nullptr;
+    }
+}
+
+void printArray(int* a, int size) {
+	cout << "[";
+	for (int i = 0; i < size; ++i) {
+		if (i == size - 1) {
+			cout << a[i] << "]";
+		}
+		else {
+			cout << a[i] << ", ";
+		}
+	}
+}
